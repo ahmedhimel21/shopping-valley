a605c4f6936a83705b0f43f3621b19be4f41b297
ahmedhimel21 <ahmedhimel000@gmail.com>
2024-04-04 14:20:33 +0600

    initial setup and navbar implemented

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/Header/Header'
+import Products from './components/Products'
+
+function App() {
+
+  return (
+    <>
+     <Header></Header>
+     <Products></Products>
+    </>
+  )
+}
+
+export default App
